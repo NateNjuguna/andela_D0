e39b07098f93f05cbd19b32788fb3095c5ed0b5e
NateNjuguna <ngrafix0faiv@gmail.com>
2017-01-17 13:34:29 +0300

    Added test for integer

--- prime.py
+++ prime.py
@@ -1,3 +1,4 @@
 def generate_prime_numbers(val):
-  if(type(val) is not type(0)):
-    raise TypeError('Value passed is not an integer')
+	if type(val) is not type(0):
+		raise TypeError('Value is not an integer');
+    
--- test_prime.py
+++ test_prime.py
@@ -2,7 +2,7 @@ import unittest;
 
 from prime import generate_prime_numbers
 
-class TestGeneratePrime(unittest.TestCase):
-  def test_input_is_integer(self):
-    with self.assertRaise(TypeError):
-      generate_prime_numbers("5")
+class GeneratePrimeTest(unittest.TestCase):
+	def test_input_is_integer(self):
+		with self.assertRaises(TypeError):
+			generate_prime_numbers("5")
