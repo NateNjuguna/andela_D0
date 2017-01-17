90ab52c2ac9fa56ad2c9e528fc0ff27758d8b2d6
NateNjuguna <ngrafix0faiv@gmail.com>
2017-01-17 14:15:28 +0300

    Added test for list output

--- prime.py
+++ prime.py
@@ -1,4 +1,11 @@
 def generate_prime_numbers(val):
 	if type(val) is not type(0):
 		raise TypeError('Value is not an integer');
-    
+	else:
+		primes = []
+		for x in range(0, val+1):
+			for y in range(1, val):
+				if x%y is 0:
+					break
+			primes.append(x)
+		return primes
--- test_prime.py
+++ test_prime.py
@@ -6,3 +6,9 @@ class GeneratePrimeTest(unittest.TestCase):
 	def test_input_is_integer(self):
 		with self.assertRaises(TypeError):
 			generate_prime_numbers("5")
+	
+	def test_output_is_list(self):
+		self.assertEquals(type([]), type(generate_prime_numbers(20)), msg="Does not produce list")
+	
+	def test_output_has_integers(self):
+		self.assertEquals(type(0), type(generate_prime_numbers(30)[2]), msg="Output list does not have integers")
